f58971a0b8e475132a5682319bb8f43e3ef802c5
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2023-12-04 13:18:26 +0200

    create doctor_image_and_text

--- lib/features/onboarding/ui/onboarding_screen.dart
+++ lib/features/onboarding/ui/onboarding_screen.dart
@@ -1,4 +1,5 @@
 import 'package:doctor_app/features/onboarding/ui/widgets/doc_logo_and_name.dart';
+import 'package:doctor_app/features/onboarding/ui/widgets/doctor_image_and_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -13,7 +14,9 @@ class OnboardingScreen extends StatelessWidget {
           child: Padding(
             padding: EdgeInsets.only(
               top: 30.h,
-              bottom: 30.h
+              bottom: 30.h,
+              left: 8.w,
+              right: 8.w,
             ),
             child: Column(
               children: [
@@ -21,7 +24,8 @@ class OnboardingScreen extends StatelessWidget {
                 SizedBox(
                   height: 30.h,
                 ),
-            //    DoctorImageAndText()
+               const DoctorImageAndText(),
+
               ],
             ),
           ),
