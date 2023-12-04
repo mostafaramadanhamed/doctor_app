d427f66a8d3c0e0e758e6a9c83866c30e84cc145
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2023-12-04 12:52:35 +0200

    add doc logo and name

---
+++ lib/core/theme/styles.dart
@@ -0,0 +1,17 @@
+import 'package:doctor_app/core/theme/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class TextStyles{
+  static TextStyle font24Black700Weight= TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+  static TextStyle font32BlueBold= TextStyle(
+    fontSize: 32.sp,
+    fontWeight: FontWeight.bold,
+    color: ColorManager.mainBlue,
+  );
+
+}
--- lib/features/onboarding/ui/onboarding_screen.dart
+++ lib/features/onboarding/ui/onboarding_screen.dart
@@ -1,12 +1,32 @@
+import 'package:doctor_app/features/onboarding/ui/widgets/doc_logo_and_name.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('onboarding'),
+    return  Scaffold(
+      body: SafeArea(
+        child:SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(
+              top: 30.h,
+              bottom: 30.h
+            ),
+            child: Column(
+              children: [
+                const DocLogoAndName(),
+                SizedBox(
+                  height: 30.h,
+                ),
+            //    DoctorImageAndText()
+              ],
+            ),
+          ),
+        ) ,
+      ),
     );
   }
 }
